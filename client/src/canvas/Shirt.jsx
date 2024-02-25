1d924777139f89a5046b2224ecbf2790c3ad3d9d
antonnprince <antonprince95@gmail.com>
2024-02-25 10:33:54 +0530

    resolved error

--- client/src/canvas/Shirt.jsx
+++ client/src/canvas/Shirt.jsx
@@ -1,23 +1,14 @@
-import {easing} from 'maath';
-import {useSnapshot} from 'valtio'
-import { useFrame } from '@react-three/fiber';
-import {Decal, useGLTF,useTexture} from '@react-three/drei'
-import state from '../store';
+/* eslint-disable react/no-unknown-property */
+/* eslint-disable no-unused-vars */
+import React from 'react'
 
 const Shirt = () => {
-  const snap = useSnapshot(state);
-  const {nodes,materials} = useGLTF('/shirt_baked.glb')
-  
-  const logoTexture = useTexture(snap.logoDecal)
-  const fullTexture = useTexture(snap.fullDecal)
-  
   return (
-    <group>
-        <mesh>
-          
-        </mesh>
-    </group>
+    <div>
+      
+    </div>
   )
 }
 
 export default Shirt
+
--- client/src/canvas/index.jsx
+++ client/src/canvas/index.jsx
@@ -1,24 +1,10 @@
-import React from 'react'
-import {Canvas} from '@react-three/fiber'
-import {Environment, Center} from '@react-three/drei'
-import Shirt from './Shirt'
-import Backdrop from './Backdrop'
-import CameraRig from './CameraRig'
+
 
 
 const CanvasModel = () => {
   return (
     <div>
-      <ambientLight 
-        intensity={0.5}
-      />
-
-      <Environment preset='city'/>
-
       
-          <Center>
-            <Shirt/>
-          </Center>
       
     </div>
   )
