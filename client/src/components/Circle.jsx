75b03d904d70d9d2c04ce26e4145ef4c02a0ddd2
antonnprince <antonprince95@gmail.com>
2024-03-01 14:46:39 +0530

    adjustments

--- client/src/components/Circle.jsx
+++ client/src/components/Circle.jsx
@@ -1,29 +1,25 @@
-import { useScroll } from '@react-three/drei'
 import React from 'react'
 import { useState } from 'react'
 
 const Circle = (props) => {
 
-    const [color,setColor] = useState('#FFFFFF')
+    const [content,showContent] = useState(false)
 
-    const handleColor=()=>{
-            
-    }
 
   return (
     <mesh {...props}
     onPointerOver={(e) => {
-        e.object.scale.set(0.5, 0.5, 0.5); 
-    e.object.material.color.set(0x636f82); 
-  }}
-  onPointerOut={(e) => {
+    e.object.scale.set(0.5, 0.5, 0.5); 
+        }}
+  
+    onPointerOut={(e) => {
     e.object.scale.set(0.4,0.4,0.4)
-    e.object.material.color.set(0xffffff); 
-  }}
-    >
-        <circleBufferGeometry args={[1, 32]} color={color}/>
-        <meshBasicMaterial />
+        }}>
+        <circleBufferGeometry args={[1, 32]} />
+        <meshBasicMaterial transparent opacity={0.7} color={0xffffff}/>
       </mesh>
+
+     
   )
 }
 
